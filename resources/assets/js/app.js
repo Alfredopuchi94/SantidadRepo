a4a4cd17df0fd06bad05b9e54fc9d3ea87339f76
Unknown <alfredopuchi94@gmail.com>
2017-10-30 18:52:15 -0400

    update app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,20 +10,43 @@
 	      changeYear: true,
 	  }
 	);
-	// o-o-o-o-o-o- ASISTE A CELULA -o-o-o-o-o-o
-		$('#lider').hide();
 
-			$("#SI").click(function(){
+	$('#lider').hide();
 
-				if( $('#SI').prop('checked') ) {
-				    $('#lider').show();
-				}else { $('#lider').hide(); }
-			});
+	$("#SI").click(function(){
 
-			$("#NO").click(function(){
+		if( $('#SI').prop('checked') ) {
+		    $('#lider').show();
+		}else { $('#lider').hide(); }
+	});
 
-				if( $('#NO').prop('checked') ) {
-				    $('#lider').hide();
-				}else { $('#lider').show(); }
-			});
-		})
+	$("#NO").click(function(){
+
+		if( $('#NO').prop('checked') ) {
+		    $('#lider').hide();
+		}else { $('#lider').show(); }
+	});
+
+	$('.ch').click(function() {
+		setTimeout(function(){
+			var cont=0;
+
+			for (var i=1; i <= 8; i++) {
+				var title = '#checkbox'+i
+				var aux = $(title)
+				if (aux.is(':checked')) {
+				  	cont = cont + 1;
+				}
+			}
+			$('#total').val(cont)
+			if ($('#total').val() > 6) {
+				$('#total').addClass('is-valid')
+				$('#total').removeClass('is-invalid')
+			}else{
+				$('#total').addClass('is-invalid')
+				$('#total').removeClass('is-valid')
+
+			}
+		}, 10);
+	})
+})
