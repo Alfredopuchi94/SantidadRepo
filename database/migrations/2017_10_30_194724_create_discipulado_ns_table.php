67e1415ba40d060dc70b82b48e3dab4eb28d2fb7
Unknown <alfredopuchi94@gmail.com>
2017-10-30 16:34:34 -0400

    cretate discipuladoN

--- database/migrations/2017_10_30_194724_create_discipulado_ns_table.php
+++ database/migrations/2017_10_30_194724_create_discipulado_ns_table.php
@@ -15,6 +15,24 @@ class CreateDiscipuladoNsTable extends Migration
     {
         Schema::create('discipulado_ns', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('Cedula');
+            $table->enum('genero', ['masculino','femenino'])->default('masculino');
+            $table->date('fechaIni');
+            $table->date('fechaFin');
+            $table->enum('clase1', ['si','no'])->default('no');
+            $table->enum('clase2', ['si','no'])->default('no');
+            $table->enum('clase3', ['si','no'])->default('no');
+            $table->enum('clase4', ['si','no'])->default('no');
+            $table->enum('clase5', ['si','no'])->default('no');
+            $table->enum('clase6', ['si','no'])->default('no');
+            $table->enum('clase7', ['si','no'])->default('no');
+            $table->enum('clase8', ['si','no'])->default('no');
+            $table->integer('total');
+            $table->string('dicipulador');
+            $table->integer('cedulaCreyente')->unsigned();
+
+            $table->foreign('CedulaCreyente')->references('cedula')->on('nuevos_creyentes')->onDelete('no action');
+            
             $table->timestamps();
         });
     }
