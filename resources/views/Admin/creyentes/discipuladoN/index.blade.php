ae40b5dae8ab5a939ff7a569dccaa88af13a509d
Unknown <alfredopuchi94@gmail.com>
2017-11-01 23:00:29 -0400

    create index

---
+++ resources/views/Admin/creyentes/discipuladoN/index.blade.php
@@ -0,0 +1,24 @@
+@extends('template.main')
+
+@section('title', 'lista de Discipulado de Nuevos Creyentes')
+
+@section('content')
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12  mr-auto ml-auto">
+			<div class="card">
+				<div class="card-header">
+						<div class="row">
+							<div class="col-md-6"><h4>NUEVOS CREYENTES</h4></div>
+							
+						</div>
+					</div>
+
+
+			</div>
+		</div>
+	</div>
+</div>
+
+@endsection
